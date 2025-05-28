e8e749307bc8309211f80323a6a01cfaca9e0ae1
Guilherme <guilherme.anm10@gmail.com>
2025-05-28 16:28:33 -0300

    .

--- Data Struct 1/Recember C/Quest 1.c
+++ Data Struct 1/Recember C/Quest 1.c
@@ -3,10 +3,15 @@
 #include <string.h>
 
 int primo(int *n){
-
+    return((*n%2!=0 && *n%3!=0 && *n%5!=0) || *n==2 || *n==3 || *n==5);
 }
 
 int main(){
-    
-
+    int n;
+    printf("Write a number:\n");
+    scanf("%d", &n);
+    if (primo(&n) == 1)
+        printf("is cousin");
+    else
+        printf("Not cousin");
 }
---
+++ Data Struct 1/Recember C/Quest 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int soma_impares(int *n){
+    return(soma_impares(*n+1));
+}
+
+int main(){
+    int n;
+    printf("Write a number:\n");
+    scanf("%d", &n);
+    printf("The sum for the numbers is %d", soma_impares(&n));
+}
